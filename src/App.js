59bbe20082dd452de5b89d40511d2973af069a3d
Chinmay Joshi <chinmay.joshi@excellarate.com>
2022-09-22 16:11:06 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import MovieList from './components/MovieList';
 function App() {
+
+  const[movies,setMovies]=useState([])
+
+ const fetchMovies=()=>{
+  fetch('https://swapi.dev/api/films/')
+  .then((response)=>{
+      return response.json();
+    }
+  )
+  .then( (data)=>{
+    const featuredFilms= data.results.map(element=>{
+      return{
+        id: element.episode_id,
+        title: element.title,
+        openingText: element.opening_crawl
+      }
+    })
+    setMovies(featuredFilms)
+
+  })
+ }
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <React.Fragment>
+      <section>
+        <button onClick={fetchMovies}>Fetch Movies</button>
+      </section>
+      <MovieList movies={movies} />
+      <section>
+      </section>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/MovieList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Movie from './Movie';
+import classes from './MovieList.module.css';
+
+const MovieList = (props) => {
+  return (
+    <ul className={classes['movies-list']}>
+      {props.movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          openingText={movie.openingText}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
